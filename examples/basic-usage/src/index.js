5554deae1f5cbbc8387790fc80fe608dd48af266
ChopperLee <lihengpro@gmail.com>
2024-10-30 11:22:55 +0800

    init

---
+++ examples/basic-usage/src/index.js
@@ -0,0 +1,6 @@
+import SmartRating from '../../../src/SmartRating'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+console.log('SmartRating: ', SmartRating)
+ReactDOM.render(<SmartRating />, document.getElementById('root'))
